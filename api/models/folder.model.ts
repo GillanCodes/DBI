c0757714c730c4dc3a4c77d766b0f03842d7aaba
GillanCodes <38374481+GillanCodes@users.noreply.github.com>
2023-12-07 10:55:52 +0100

    Create folder.model.ts

---
+++ api/models/folder.model.ts
@@ -0,0 +1,17 @@
+import { Schema, model } from "mongoose"
+
+export interface IFolder extends Document
+{
+    name:           string,
+    description:    string,
+    icon:           string
+}
+
+const folderSchema = new Schema<IFolder>({
+    name: {type:String, required:true},
+    description: {type:String, required:true},
+    icon:{type:String, default:"none"}
+});
+
+const folderModel = model<IFolder>('folder', folderSchema);
+export default folderModel;
