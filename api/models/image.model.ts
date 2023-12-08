80ee491d98d60b045beae1705a79895a60d25016
GillanCodes <38374481+GillanCodes@users.noreply.github.com>
2023-12-08 11:08:04 +0100

    added category && property

--- api/models/image.model.ts
+++ api/models/image.model.ts
@@ -1,16 +1,24 @@
 import { Schema, model } from "mongoose";
 
+export interface IProperty
+{
+    type: string,
+    value: string
+}
+
 export interface IImage extends Document
 {
     folderId: string,
     filePath:string,
     tags:[string],
+    category:string
 }
 
 const imageSchema = new Schema<IImage>({
     folderId:   {type:String, required:true},
     filePath:   {type:String, required:true},
-    tags:       {type:[String]}
+    tags:       {type:[String]},
+    category:   {type:String}
 });
 
 const imageModel = model<IImage>('image', imageSchema);
