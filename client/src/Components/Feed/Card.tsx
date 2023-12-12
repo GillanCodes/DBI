b77850fcd38c5ba84223b964fe227a9b30b129e1
GillanCodes <38374481+GillanCodes@users.noreply.github.com>
2023-12-12 11:14:16 +0100

    cards basis done

--- client/src/Components/Feed/Card.tsx
+++ client/src/Components/Feed/Card.tsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IImage } from '../../types'
 
 export default function Card({image} : {image:IImage}) {
+
+  const [clicked, setClicked] = useState(false);
+
   return (
-    <div style={{overflow:"scroll"}}>
-        <div className="head">
+    <div className='card'>
+        <div className="card-head">
 
         </div>
-        <div className="body">
-          <img src={`${process.env.REACT_APP_CDN_URL}/uploads/${image.filePath}`} alt="" />
+        <div className="card-body">
+          <img 
+            className={clicked ? "card-image full-width" : "card-image full-height"} 
+            src={`${process.env.REACT_APP_CDN_URL}/uploads/${image.filePath}`} 
+            alt="" 
+            onClick={() => setClicked(!clicked)}
+          />
+        </div>
+        <div className="card-footer">
+          
         </div>
     </div>
   )
