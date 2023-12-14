7af349562d5cc11e3c7c110e2bc774e7c5fd8b86
GillanCodes <38374481+GillanCodes@users.noreply.github.com>
2023-12-14 11:01:53 +0100

    Update Random.tsx

--- client/src/Components/Random/Random.tsx
+++ client/src/Components/Random/Random.tsx
@@ -3,7 +3,8 @@ import { useEffect, useState } from "react"
 import { isEmpty } from "../../Utils";
 import Dropdown from "../Utils/Dropdown";
 import { useSelector } from "react-redux";
-import { IFolder, IState } from "../../types";
+import { IFolder, IImage, IState } from "../../types";
+import ImageSettings from "../Image/ImageSettings";
 
 export default function Random() {
 
@@ -12,10 +13,14 @@ export default function Random() {
 
     //Pics
     const [img, setImg] = useState('');
+    const [imgData, setImgData] = useState();
     const [previus, setPrevius] = useState('');
 
     //Store all the previus pic
     const [history, setHistory]:any = useState([]);
+
+    //Open modal
+    const [modal, setModal] = useState(false);
 
     //folder selector
     const folders = useSelector((state:IState) => state.foldersReducer);
@@ -27,6 +32,7 @@ export default function Random() {
             url: `${process.env.REACT_APP_API_URL}/random/`,
             withCredentials:true
         }).then((res) => {
+            setImgData(res.data);
             setImg(res.data.filePath);
             setHistory([{name: folders.find((folder:IFolder) => folder._id === res.data.folderId)?.name, value: res.data.filePath}, ...history]);
         })
@@ -61,23 +67,30 @@ export default function Random() {
 
     return (
         <div className="container">
-            <div className="random">
-                <div className="top-bar">
-                    <p className="button" onClick={prevHandle}>Prev</p>
-                    <p className="button" onClick={nextHandle}>Next</p>
-                    {!isEmpty(history) && (
-                        <Dropdown id="his" title="history" setCurrentValue={setImg} currentValue={img} items={history} />
-                    )}
-                </div>
+            <>
+                <div className="random">
+                    <div className="top-bar">
+                        <p className="button" onClick={prevHandle}>Prev</p>
+                        <p className="button" onClick={nextHandle}>Next</p>
+                        <p className="button" onClick={() => setModal(!modal)}>Settings - {modal ? "o" : "n"}</p>
+                        {!isEmpty(history) && (
+                            <Dropdown id="his" title="history" setCurrentValue={setImg} currentValue={img} items={history} />
+                        )}
+                    </div>
 
-                <div className="display">
-                    {!isEmpty(img) && (
-                        <img src={`${process.env.REACT_APP_CDN_URL}/uploads/${img}`} alt="image"></img>
-                    )}
-                    
+                    <div className="display">
+                        {!isEmpty(img) && (
+                            <img src={`${process.env.REACT_APP_CDN_URL}/uploads/${img}`} alt="image"></img>
+                        )}
+                        
+                    </div>
                 </div>
-            </div>
-
+                {modal && (
+                    <div className="modal">
+                        <ImageSettings image={imgData} close={setModal} />
+                    </div>
+                )}
+            </>
 
         </div>
     )
