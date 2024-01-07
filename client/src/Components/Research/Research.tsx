d0152f070ca306022d61d5bc7bf2b59368829916
GillanCodes <38374481+GillanCodes@users.noreply.github.com>
2024-01-07 11:11:58 +0100

    feat(client) : added search by media type

--- client/src/Components/Research/Research.tsx
+++ client/src/Components/Research/Research.tsx
@@ -5,12 +5,14 @@ import axios from 'axios';
 import { IImage } from '../../types';
 import { isEmpty } from '../../Utils';
 import MediaGrid from '../Utils/MediaGrid';
+import Dropdown from '../Utils/Dropdown';
 
 export default function Research() {
 
     const [modal, setModal] = useState<boolean>(false);
     const [tags, setTags] = useState<string[]>([]);
     const [category, setCategory] = useState<string>("");
+    const [type, setType] = useState<string>("");
     
     const [count, setCount]     = useState(24);
     const [loadImg, setLoadImg] = useState(true);
@@ -21,14 +23,14 @@ export default function Research() {
     useEffect(() => {
         axios({
             method: "GET",
-            url: `${process.env.REACT_APP_API_URL}/image/params?tags=${tags}&category=${category}`,
+            url: `${process.env.REACT_APP_API_URL}/image/params?tags=${tags}&category=${category}&type=${type}`,
             withCredentials: true
         }).then((res) => {
             setCount(24);
             setQuery(res.data)
             setImages(res.data.slice(0,count))
         })
-    }, [tags, category]);
+    }, [tags, category, type]);
     
      const loadMore = () => {
         if (window.innerHeight + document.documentElement.scrollTop + 1 > document.scrollingElement!.scrollHeight) 
@@ -49,12 +51,17 @@ export default function Research() {
         return () => window.removeEventListener('scroll', loadMore);
     }, [loadImg, count, images, query])
 
-
     return (
         <div className='container'>
             <div className="research">
                 <div className="head">
-                    <input type="text" className='input' onChange={(e) => setCategory(e.target.value)} />
+                    <input type="text" className='input' onChange={(e) => setCategory(e.target.value)} placeholder='Category' />
+                    <input type='text' className='input' list='type' placeholder='Type' onChange={(e) => setType(e.target.value)} />
+                    <datalist id='type'>
+                        <option value="">Tous</option>
+                        <option value="video">Videos</option>
+                        <option value="img">Images</option>
+                    </datalist>
                     <p className='button' onClick={() => setModal(!modal)}>Tags</p>
                 </div>
                 <div className="body">
