0f5da2ba4c9030edb86ce5eae0e0cbc2e96d909e
GillanCodes <38374481+GillanCodes@users.noreply.github.com>
2024-01-12 11:23:47 +0100

    feat (client) : added like btn to random

--- client/src/Components/Random/SideSettings.tsx
+++ client/src/Components/Random/SideSettings.tsx
@@ -1,12 +1,14 @@
 import { isEmpty } from "../../Utils";
+import LikeButton from "../Utils/LikeButton";
 
-export default function SideSettings({settingsModal, setSettingsModal, history, img, setImg} : {settingsModal:any, setSettingsModal:any, history:any, img:any, setImg:any}) {
+export default function SideSettings({settingsModal, setSettingsModal, history, img, setImg, imgData} : {settingsModal:any, setSettingsModal:any, history:any, img:any, setImg:any, imgData:any}) {
   return (
     <div className="settings-panel">
         <h2 className="panel-title">Settings</h2>
 
         <div className="fields">
             <p className="button" onClick={() => setSettingsModal(!settingsModal)}>Settings</p>
+            <LikeButton media={imgData} />
         </div>
 
         <div className="spacer"></div>
