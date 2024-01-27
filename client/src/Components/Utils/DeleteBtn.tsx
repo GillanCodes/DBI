ba76d380b966f12a651fbc4fd00a62fb4da0b171
GillanCodes <38374481+GillanCodes@users.noreply.github.com>
2024-01-27 12:44:41 +0100

    feat (client) toast on modal

--- client/src/Components/Utils/DeleteBtn.tsx
+++ client/src/Components/Utils/DeleteBtn.tsx
@@ -1,15 +1,28 @@
 import React from 'react'
 import { useDispatch } from 'react-redux';
 import { deleteMedia } from '../../actions/media.actions';
+import { useToasts } from './Toast/ToastContext';
 
 export default function DeleteBtn({itemId} : {itemId:string}) {
 
     const dispatch:any = useDispatch();
+    const { pushToast } = useToasts();
 
     const deleteHandle = () => {
         dispatch(deleteMedia(itemId));
+        pushToast({
+            title: "Deleting ...",
+            content : "Delete in progress ...",
+            duration: 1
+        });
         const timer = setTimeout(() => {
-            window.location.assign('/');
+            window.history.back();
+            pushToast({
+                title:"Deleted !",
+                content: "Media was delete successfully !",
+                type: "success",
+                duration: 5
+            })
         }, 1000);
 
         return () => {
