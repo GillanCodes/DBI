376f29bacf9936c072b9309c10b5af112e80d6ae
GillanCodes <38374481+GillanCodes@users.noreply.github.com>
2024-01-31 10:12:01 +0100

    feat (client) : added categories reducer && action

--- client/src/actions/category.action.ts
+++ client/src/actions/category.action.ts
@@ -1,12 +1,63 @@
 import axios from "axios";
 
 export const GET_CATEGORIES = "GET_CATEGORIES";
+export const CREATE_CATEGORIES = "CREATE_CATEGORIES";
+export const UPDATE_CATEGORIES = "UPDATE_CATEGORIES";
+export const DELETE_CATEGORIES = "DELETE_CATEGORIES";
 
 export const getCategories = () => {
     return (dispatch:any) => {
         return axios({
             method:"get",
             url:`${process.env.REACT_APP_API_URL}/category/`,
+            withCredentials:true
+        }).then((res) => {
+            dispatch({type: GET_CATEGORIES, payload: res.data});
+        }).catch((err) => {
+            console.log(err);
+        });
+    };
+};
+
+export const createCategories = (name:string) => {
+    return (dispatch:any) => {
+        return axios({
+            method:"POST",
+            url:`${process.env.REACT_APP_API_URL}/category/`,
+            withCredentials:true,
+            data: {
+                name
+            }
+        }).then((res) => {
+            dispatch({type: GET_CATEGORIES, payload: res.data});
+        }).catch((err) => {
+            console.log(err);
+        });
+    };
+};
+
+export const updateCategories = (name:string, id:string) => {
+    return (dispatch:any) => {
+        return axios({
+            method:"PATCH",
+            url:`${process.env.REACT_APP_API_URL}/category/${id}`,
+            withCredentials:true,
+            data: {
+                name
+            }
+        }).then((res) => {
+            dispatch({type: GET_CATEGORIES, payload: res.data});
+        }).catch((err) => {
+            console.log(err);
+        });
+    };
+};
+
+export const deleteCategories = (id:string) => {
+    return (dispatch:any) => {
+        return axios({
+            method:"DELETE",
+            url:`${process.env.REACT_APP_API_URL}/category/${id}`,
             withCredentials:true
         }).then((res) => {
             dispatch({type: GET_CATEGORIES, payload: res.data});
--- client/src/reducers/categoryReducer.ts
+++ client/src/reducers/categoryReducer.ts
@@ -1,4 +1,5 @@
-import { GET_CATEGORIES } from "../actions/category.action";
+import { CREATE_CATEGORIES, DELETE_CATEGORIES, GET_CATEGORIES, UPDATE_CATEGORIES } from "../actions/category.action";
+import { ICategory } from "../types";
 
 const initialState:object = {};
 
@@ -8,6 +9,15 @@ export default function categoryReducer(state:any = initialState, action:any)
     {
         case GET_CATEGORIES:
             return action.payload;
+        case CREATE_CATEGORIES:
+            return [action.payload, ...state];
+        case UPDATE_CATEGORIES:
+            return state.map((category:ICategory) => {
+                if (category._id === action.payload._id) return action.payload;
+                else return category;
+            });
+        case DELETE_CATEGORIES:
+            return state.filter((cat:ICategory) => cat._id !== action.payload._id);
         default:
             return state;
     }
